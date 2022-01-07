80ab05fa3503f8454593bf9fb69d751617feff94
Pedro Lucas <pedrolucasr936@gmail.com>
2022-01-07 17:46:42 -0300

    projeto contaBancaria

---
+++ src/application/banco.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class banco {
+
+	public static void main(String[] args) {
+				
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+	    int x;
+	    int x = sc.nextInt();	
+        System.out.println("Digite o número da conta: ");
+
+        
+     sc.close();   
+	}
+
+}
+
+	
